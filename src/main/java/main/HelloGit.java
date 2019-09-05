e1e132ee4a057f1c2c63a7a50728dc74360f2b9d
Alvaro Clemente <clemente.varo@gmail.com>
2019-09-05 22:34:26 +0200

    Print with colors! (Using JCDP)

--- src/main/java/main/HelloGit.java
+++ src/main/java/main/HelloGit.java
@@ -1,7 +1,24 @@
 package main;
 
+import com.diogonunes.jcdp.color.ColoredPrinter;
+import com.diogonunes.jcdp.color.api.Ansi;
+
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
+        System.out.println("\nTesting Dependencies");
+        printBlue("This should be white and blue!");
+        System.out.println("This should be normal!");
+    }
+
+    private static void printBlue(String text) {
+        // https://github.com/dialex/JCDP this is the library
+        ColoredPrinter p = new ColoredPrinter.Builder(1, false)
+                .foreground(Ansi.FColor.WHITE)
+                .background(Ansi.BColor.BLUE)
+                .build();
+
+        p.println(text);
+        p.clear();
     }
 }
